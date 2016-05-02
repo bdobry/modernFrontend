d255cdb531f88a1ded38fa87c16599dbcfacca98
bdobry <bartoszdobry@gmail.com>
2016-05-02 02:15:04 +0200

    routing + form validation working

--- js/app.js
+++ js/app.js
@@ -1,93 +1,55 @@
-(function(){
-  var app = angular.module('store', []);
+(function () {
+  var app = angular.module('store1', ['ngRoute', 'ngResource']);
+  var app2 = angular.module('store2', ['ngMessages']);
+  var app3 = angular.module('store', ['store1', 'store2']);
 
-  app.controller('CartController', function(){
+  app.controller('StoreController', function () {
     this.product = gem;
   });
+  
+  app2.controller('mainController', function ($scope) {
+
+  // function to submit the form after all validation has occurred			
+  $scope.submitForm = function () {
+
+    // check to make sure the form is completely valid
+    if ($scope.contactForm.$valid) {
+      alert('our form is amazing');
+    }
+
+  };
+
+});
 
   var gem = [
-  {
-    name: 'Pebbles', 
-    quantity: 5, 
-    price: 6.95,
+    {
+      name: 'Pebbles',
+      quantity: 5,
+      price: 6.95,
   },
-  {
-  name: 'Angular Chiffon Printed',
-  price: 41.19,
-  description: '',
-  }
-  ];
+    {
+      name: 'Angular Chiffon Printed',
+      price: 41.19,
+      description: '',
+  }];
+
+  app.config(function ($routeProvider) {
+    $routeProvider.
+    when('/', {
+      templateUrl: 'main.html',
+      controller: 'StoreController'
+    }).
+    when('/product/:adultId', {
+      templateUrl: 'adult-detail.html',
+      controller: 'StoreController'
+    }).
+    when('/contact', {
+      templateUrl: 'contact.html',
+      controller: 'mainController'
+    }).
+    otherwise({
+      redirectTo: '/adults'
+    });
+  });
 
 })();
-
-
-//function CartCtrl($scope) {
-//  $scope.items = [
-//    {title: 'Paint pots', quantity: 8, price: 3.95},
-//    {title: 'Polka dots', quantity: 17, price: 12.95},
-//    {title: 'Pebbles', quantity: 5, price: 6.95}
-//  ];
-//  $scope.remove = function(index) {
-//    $scope.items.splice(index,1);
-//  };
-//  $scope.in= { 
-//    title: 'Title',
-//    quantity:0,
-//    price:0
-//  };
-//  $scope.addItem = function () {
-//    $scope.items.push($scope.in);
-//    $scope.in = {
-//      title: 'Title',
-//      quantity:0,
-//      price:0
-//    };
-//  };
-//};
-
-//(function(angular) {
-//  var app = angular.module("contact", []);
-//
-//  app.controller("contactForm", ['$scope', '$http', function($scope, $http) {
-//    $scope.success = false;
-//    $scope.error = false;
-//
-//    $scope.sendMessage = function( input ) {
-//      $http({
-//          method: 'POST',
-//          url: '/processForm.php',
-//          data: input,
-//          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-//      })
-//      .success( function(data) {
-//        if ( data.success ) {
-//          $scope.success = true;
-//        } else {
-//          $scope.error = true;
-//        }
-//      } );
-//    }
-//  }]);
-//})(angular);
-
-//(function(){
-//  var app = angular.module('store', []);
-//
-//  app.controller('StoreController', function(){
-//    this.product = gem;
-//  });
-//
-//  var gem = [
-//  {
-//    name: 'Printed Chiffon Angular',
-//    price: 61.19,
-//    description: '',
-//  },
-//  {
-//  name: 'Angular Chiffon Printed',
-//  price: 41.19,
-//  description: '',
-//  }
-//  ];
-//
-//})();
